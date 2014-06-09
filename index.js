5b8e16a829b523ae1a308de9f9d4a0bcde597820
Zlatko Fedor <zfedor@gmail.com>
2014-06-09 23:55:39 +0200

    first version

---
+++ index.js
@@ -0,0 +1,59 @@
+(function (root) {
+
+	var prepare = function(Ember) {
+		Ember.whenNotRunning = function(name, original) {
+			if(typeof name === 'function') {
+				original = name;
+				name = null;
+			}
+
+			name = name || 'isRunning';
+
+			return function() {
+				var self = this;
+
+				if(this.get(name)) {
+					return false;
+				}
+
+				this.set(name, true);
+
+				//prepare arguments
+				var end = function() {
+					self.set(name, false);
+				};
+
+				var args = Array.prototype.slice.call(arguments, 0);
+				args.unshift(end);
+
+				//call original fn
+				original.apply(this, args);
+			};
+		};
+
+		if (Ember.EXTEND_PROTOTYPES === true) {
+			Function.prototype.whenNotRunning = function(name) {
+				return Ember.whenNotRunning(name, this);
+			};
+		}
+	};
+
+
+	//AMD
+	if (typeof define !== 'undefined' && define.amd) {
+		define(['ember'], function(Ember) {
+			return prepare(Ember);
+		});
+	}
+
+	//CommonJS
+	else if (typeof module !== 'undefined' && module.exports) {
+		module.exports = prepare(require('ember'));
+	}
+
+	//Script tag
+	else {
+		prepare(root.Ember);
+	}
+
+} (this));
